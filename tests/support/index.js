1a3260d72ab06e27c340b228d605235988f8f869
Joyce Pontes <joycepontesf@gmail.com>
2025-08-03 13:56:37 -0300

    Fix to use Playwright's native functions and adjustments for app actions

--- tests/support/index.js
+++ tests/support/index.js
@@ -1,19 +1,21 @@
 const { test: base, expect } = require('@playwright/test')
 
-const { Login } = require('../pages/Login')
-const { Toast } = require('../pages/Components')
-const { Movies } = require('../pages/Movies')
-const { LandingPage } = require('../pages/LandingPage')
+const { Login } = require('../actions/Login')
+const { Toast } = require('../actions/Components')
+const { Movies } = require('../actions/Movies')
+const { Leads } = require('../actions/Leads')
 
 const test = base.extend({
     page: async ({ page }, use) => {
-        await use({
-            ...page,
-            landing: new LandingPage(page),
-            login: new Login(page),
-            movies: new Movies(page),
-            toast: new Toast(page)
-        })
+
+        const context = page
+
+        context['leads'] = new Leads(page),
+        context['login'] = new Login(page),
+        context['movies'] = new Movies(page),
+        context['toast'] = new Toast(page)
+
+        await use(context)
     }
 })
 
